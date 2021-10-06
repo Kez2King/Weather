74c524dd32a947e2f90f70c18dad7e2553257819
Mar'Kez <MarkezBJC@gmail.com>
2021-10-06 18:27:53 -0400

    Background Update

--- apps.js
+++ apps.js
@@ -1,54 +1,3 @@
-// Animated Background
-
-const canvas = document.getElementById("animatedBack")
-const ctx = canvas.getContext('2d')
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
-
-let particlesArray;
-// Get Mouse position
-let mouse = {
-  x = null,
-  y = null,
-  radius: (canvas.height/80) * (canvas.width/80)
-}
-window.addEventListener("mouseover", function(event){
-  mouse.x = event.x
-  mouse.y = event.y
-})
-// Create particles
-class Particles {
-  constructor(x, y, directionX, directionY, size, color){
-    this.x = x
-    this.y = y
-    this.directionX = directionX
-    this.directionY = directionY
-    this.size = size
-    this.color = color
-  }
-  // Methos to draw to particles
-  draw (){
-    ctx.beginPath()
-    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false)
-    ctx.fillStyle = '#8C5523'
-    ctx.fill()
-  }
-  // Check Positive, mouse position, moveover,
-  // draws particles
-  update(){
-    if (this.x > canvas.width || this.x < 0) {
-      this.directionX = -this.directionX
-    }
-    if (this.y > canvas.height || this.y < 0) {
-      this.directionY = -this.directionY
-    }
-    // Check collision detection
-    let dx = mouse.x - this.x
-    let dy = mouse.y - this.y
-    let distance = Math.sqrt(dx*dx + dy*dy)
-  }
-}
-
 //fetch data from weather api
 
 function weatherData(cityID) {
@@ -82,3 +31,123 @@ function drawWeather(d) {
   document.getElementById("temp").innerHTML = celcius + "&deg;";
   document.getElementById("location").innerHTML = d.name;
 }
+
+// Animated Background
+const canvas = document.getElementById("animatedBack")
+const ctx = canvas.getContext('2d')
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+let particlesArray;
+// Get Mouse position
+let mouse = {
+  x: null ,
+  y: null ,
+  radius: (canvas.height/80) * (canvas.width/80)
+}
+window.addEventListener("mouseover",
+  function(event){
+  mouse.x = event.x
+  mouse.y = event.y
+})
+// Create particles
+class Particles {
+  constructor(x, y, directionX, directionY, size, color){
+    this.x = x
+    this.y = y
+    this.directionX = directionX
+    this.directionY = directionY
+    this.size = size
+    this.color = color
+  }
+  // Methos to draw to particles
+  draw (){
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false)
+    ctx.fillStyle = '#8C5523'
+    ctx.fill()
+  }
+  // Check Positive, mouse position, moveover,
+  // draws particles
+  update(){
+    if (this.x > canvas.width || this.x < 0) {
+      this.directionX = -this.directionX
+    }
+    if (this.y > canvas.height || this.y < 0) {
+      this.directionY = -this.directionY
+    }
+    // Check collision detection
+    let dx = mouse.x - this.x
+    let dy = mouse.y - this.y
+    let distance = Math.sqrt(dx*dx + dy*dy)
+    if (distance < mouse.radius + this.size){
+      if (mouse.x < this.x && this.x < canvas.width - this.size * 10) {
+        this.x += 10
+      }
+      if (mouse.x > this.x && this.x > canvas.width * 10) {
+        this.x -= 10
+      }
+      if (mouse.y < this.y && this.y < canvas.height - this.size * 10) {
+        this.x += 10
+      }
+      if (mouse.y > this.y && this.y > canvas.height * 10) {
+        this.x -= 10
+      }
+    }
+    // Moves Particles
+    this.x += this.directionX
+    this.y += this.directionY
+    // Draw Particles
+    this.draw()
+    
+  }
+}
+// Create Particle Array
+function init(){
+  particlesArray = []
+  let numberofParticles = (canvas.height * canvas.width) / 9000
+  for (let i = 0; i < numberofParticles; i++) {
+    let size = (Math.random() * 5) + 1
+    let x = (Math.random() * ((innerWidth - size * 2) - (size * 2)) + size * 2)
+    let y = (Math.random() * ((innerHeight - size * 2) - (size * 2)) + size * 2)
+    let directionX = (Math.random() * 5) - 2.5
+    let directionY = (Math.random() * 5) - 2.5
+    let color = '#8C5523'
+
+    particlesArray.push(new Particles(x, y, directionX, directionY, size, color))
+  }
+}
+// Animation Loop
+function animate(){
+  requestAnimationFrame(animate)
+  ctx.clearRect(0, 0, innerWidth, innerHeight)
+
+  for (let i = 0; i < particlesArray.length; i++) {
+    particlesArray[i].update()
+  }
+  connect()
+}
+
+// Check if particles are close enough to draw lines
+function connect(){
+  for (let a = 0; a < particlesArray.length; a++) {
+    for (let b = 0; b < particlesArray.length; b++) {
+      let distance = ((particlesArray[a].x - particlesArray[b].x)
+      * (particlesArray[a].x - particlesArray[b].x)
+      + (particlesArray[a].y - particlesArray[b].y)
+      * (particlesArray[a].y - particlesArray[b].y)
+      )
+
+      if ((distance < canvas.width/7) * (distance < canvas.height/7)){
+        ctx.strokeStyle = 'rgba(140,85,31,1)'
+        ctx.lineWidth = 1
+        ctx.beginPath()
+        ctx.moveTo(particlesArray[a].x, particlesArray[a].y)
+        ctx.lineTo(particlesArray[b].x, particlesArray[b].y)
+        ctx.stroke()
+      }
+    }
+  }
+}
+init();
+animate();
