e8e5d918a015876f42e893bf6d95ea722fbd2d33
Mar'Kez <MarkezBJC@gmail.com>
2021-10-06 13:58:10 -0400

    Particle Background

--- apps.js
+++ apps.js
@@ -1,3 +1,54 @@
+// Animated Background
+
+const canvas = document.getElementById("animatedBack")
+const ctx = canvas.getContext('2d')
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+let particlesArray;
+// Get Mouse position
+let mouse = {
+  x = null,
+  y = null,
+  radius: (canvas.height/80) * (canvas.width/80)
+}
+window.addEventListener("mouseover", function(event){
+  mouse.x = event.x
+  mouse.y = event.y
+})
+// Create particles
+class Particles {
+  constructor(x, y, directionX, directionY, size, color){
+    this.x = x
+    this.y = y
+    this.directionX = directionX
+    this.directionY = directionY
+    this.size = size
+    this.color = color
+  }
+  // Methos to draw to particles
+  draw (){
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false)
+    ctx.fillStyle = '#8C5523'
+    ctx.fill()
+  }
+  // Check Positive, mouse position, moveover,
+  // draws particles
+  update(){
+    if (this.x > canvas.width || this.x < 0) {
+      this.directionX = -this.directionX
+    }
+    if (this.y > canvas.height || this.y < 0) {
+      this.directionY = -this.directionY
+    }
+    // Check collision detection
+    let dx = mouse.x - this.x
+    let dy = mouse.y - this.y
+    let distance = Math.sqrt(dx*dx + dy*dy)
+  }
+}
+
 //fetch data from weather api
 
 function weatherData(cityID) {
